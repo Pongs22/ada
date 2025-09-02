ae66c14b505b765da702c2f6a8814ee618e14dc5
junmar2 <junmar@greydientlab.com>
2025-09-03 03:59:15 +0800

    updated layouts

--- footer.php
+++ footer.php
@@ -14,15 +14,17 @@
 <footer id="colophon" class="relative bg-ada_red-70 leading-6 text-gray-600">
 	<img src="<?php echo esc_url( get_stylesheet_directory_uri() . '/frontend/resources/img/ada-footer-bg-dt.jpg' ); ?>"
 		class="absolute left-0 top-0 h-full w-full object-cover object-right-top">
-	<div class="ff-c-container relative z-10 flex flex-col gap-y-10 py-10">
-		<div class="site-branding size-[160px]">
-			<?php
-			the_custom_logo();
-			?>
+	<div class="px-6 md:px-8 lg:px-10 xl:px-20">
+		<div class="ff-c-container relative z-10 flex flex-col gap-y-10 py-10">
+			<div class="site-branding size-[160px]">
+				<?php
+				the_custom_logo();
+				?>
+			</div>
+			<p class="max-w-[296px] text-base text-white">9 Straits View, Marina One West Tower, Singapore 018937</p>
 		</div>
-		<p class="max-w-[296px] text-base text-white">9 Straits View, Marina One West Tower, Singapore 018937</p>
 	</div>
-	<div class="relative z-10 bg-[#d6d6d6]/10 px-8 py-3">
+	<div class="relative z-10 bg-[#d6d6d6]/10 px-6 py-3 md:px-8 lg:px-10 xl:px-20">
 		<div class="ff-c-container flex flex-row justify-between">
 			<p class="my-auto text-base leading-[120%] text-white">© 2025 Apex Digital Academy. All rights reserved.</p>
 			<div class="h-12"></div>
